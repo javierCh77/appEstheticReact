e647ebd8e6fc0d0394e56e3d82a2f203e50c01e5
ushlinxdev <cardansel@outlook.com>
2023-09-27 20:34:02 -0300

    Add Section

---
+++ dr-ruth/src/components/sections/section1/Section1.jsx
@@ -0,0 +1,9 @@
+import { Card } from '@mui/material';
+
+export default function Section1() {
+    return (
+        <Card>
+            <div className="section-title"></div>
+        </Card>
+    )
+}
