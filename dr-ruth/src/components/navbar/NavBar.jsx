8fc2d5be5e0ba27e0005dde7332cd5049abea411
javierCh77 <javierchavarria21@gmail.com>
2023-09-27 09:29:13 -0300

    all

--- dr-ruth/src/components/navbar/NavBar.jsx
+++ dr-ruth/src/components/navbar/NavBar.jsx
@@ -1,8 +1,11 @@
 import * as React from 'react';
 import './navBar.css'
 import { Link } from 'react-router-dom'
-
-
+//import logos
+import logo3 from '../../assets/icon/logo3.png'
+import instagram from '../../assets/icon/instagram.svg'
+import facebook from '../../assets/icon/facebook.svg'
+import twitter from '../../assets/icon/twitter.svg'
 
 export default function NavBar() {
 
@@ -10,8 +13,7 @@ export default function NavBar() {
     <div className='container-nav'>
     <nav class="navbar fixed-top ">
        <div className='container-img'>
-            aqui el logo
-        <img src="" alt="" />
+        <img src={logo3} width="50" alt="" />
        </div>
         <div class="container-link">
            <Link class="link-nav" to={"/inicio"}>Inicio</Link>
@@ -21,7 +23,9 @@ export default function NavBar() {
            <Link class="link-nav" to={"/contacto"}>Contacto</Link>
         </div>
         <div className='container-social'>
-            redes
+        <Link ><img src={instagram} alt=""></img></Link>
+        <Link ><img src={facebook} alt=""></img></Link>
+        <Link ><img src={twitter} alt=""></img></Link>
         </div>
     </nav>
     </div>
