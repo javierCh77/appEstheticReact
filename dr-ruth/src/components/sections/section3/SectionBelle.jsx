673d606680b3d6b9c4e99a1b6555878c03fab654
ushlinxdev <cardansel@outlook.com>
2023-09-28 00:05:34 -0300

    Add Section 2 y 3

---
+++ dr-ruth/src/components/sections/section3/SectionBelle.jsx
@@ -0,0 +1,10 @@
+
+import './SectionBelle.css';
+
+export default function SectionBelle() {
+    return (
+        <div className="container-text mt-2">
+              <p className='text'>La belleza comienza en el momento en que decides ser tú misma</p>
+       </div>
+    );
+}
