35b9e42a0b703e760ee3557269589b293e8a881b
cardansel <cardansel@gmail.com>
2023-10-03 23:39:35 -0300

    Update Home with Carousel Autoplay

--- dr-ruth/src/views/Home.jsx
+++ dr-ruth/src/views/Home.jsx
@@ -10,15 +10,44 @@ import Galeria from "../components/sections/section4/Galeria";
 import Section5 from "../components/sections/section5/Section5";
 import Contacto from "../components/sections/section6/Contacto";
 
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/css";
+// import required modules
+import { Autoplay } from "swiper/modules";
+
+import Slide01 from "../assets/carousel/c1.jpg";
+import Slide02 from "../assets/carousel/c2.jpg";
+import Slide03 from "../assets/carousel/c3.jpg";
 
 export default function Home() {
   return (
     <div className="container-home">
-       <div className="container-hero" id="inicio">
-           <div className="container-na">
-             <NavBar />
-           </div>
+      <div className="container-hero" id="inicio">
+        <div className="container-na">
+          <NavBar />
         </div>
+        <Swiper
+          spaceBetween={30}
+          centeredSlides={true}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          modules={[Autoplay]}
+        >
+          <SwiperSlide>
+            <img src={Slide01} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={Slide02} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={Slide03} alt="" />
+          </SwiperSlide>
+        </Swiper>
+      </div>
       <div className="section1">
         <Section1 />
       </div>
@@ -32,15 +61,14 @@ export default function Home() {
         <Galeria />
       </div>
       <div className="section5">
-          <Section5/>
+        <Section5 />
       </div>
       <div className="section6">
-          <Contacto/>
+        <Contacto />
       </div>
       <div className="container-footer">
         <Footer />
       </div>
-  
     </div>
   );
 }
